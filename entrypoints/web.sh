6eecc7fb7b5b4695f338aefd02ede5e80a69f96a
Audacious Tux <tangimhossain1@gmail.com>
2024-06-19 05:22:34 +0000

    remove unneeded files

--- entrypoints/web.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# wait untill the db is fully up before proceeding
-while [[ $(mysqladmin --host=$MAUTIC_DB_HOST --port=$MAUTIC_DB_PORT --user=$MAUTIC_DB_USER --password=$MAUTIC_DB_PASSWORD ping) != "mysqld is alive" ]]; do
-	sleep 1
-done
-
-apache2-foreground
