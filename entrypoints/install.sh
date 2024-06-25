210283a21d8106a0adc0af6d5eb820dbf4f20a6a
Audacious Tux <tangimhossain1@gmail.com>
2024-06-25 06:28:21 +0000

    ..

--- entrypoints/install.sh
+++ entrypoints/install.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
+config_file=config/local.php
+
+if [ -f $config_file ]; then
+    echo $config_file already exists, skipping installation...
+    exit 0
+fi
+
 echo "Creating database ${MAUTIC_DB_NAME:?}, if it does not already exist..."
 while ! mysql -h ${MAUTIC_DB_HOST:?} -u ${MAUTIC_DB_USER:?} -p${MAUTIC_DB_PASSWORD:?} -e "CREATE DATABASE IF NOT EXISTS ${MAUTIC_DB_NAME:?}"; do
     sleep 5
 done
 
-config_file=config/local.php
+php bin/sync_config.php
 
-if [ -f $config_file ]; then
-    echo $config_file already exists, skipping installation...
-else
-    php bin/sync_config.php
-
-    php bin/console mautic:install \
-        --no-interaction \
-        --force \
-        ${MAUTIC_SITE_URL:?} 
-fi
+php bin/console mautic:install \
+    --no-interaction \
+    --force \
+    "$@"
