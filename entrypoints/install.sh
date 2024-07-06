809c45c976c27d0e675c7750af461de8b330d011
Audacious Tux <tangimhossain1@gmail.com>
2024-07-06 18:23:21 +0000

    ..

--- entrypoints/install.sh
+++ entrypoints/install.sh
@@ -5,15 +5,9 @@ set -e
 echo "Creating database ${MAUTIC_DB_NAME:?}, if it does not already exist..."
 mysql -h ${MAUTIC_DB_HOST:?} -u ${MAUTIC_DB_USER:?} -p${MAUTIC_DB_PASSWORD:?} -e "CREATE DATABASE IF NOT EXISTS ${MAUTIC_DB_NAME:?}"
 
+php bin/sync_config.php
+
 php bin/console mautic:install \
-    --db-host=${MAUTIC_DB_HOST:?} \
-    --db-port=${MAUTIC_DB_PORT:?} \
-    --db-name=${MAUTIC_DB_NAME:?} \
-    --db-user=${MAUTIC_DB_USER:?} \
-    --db-password=${MAUTIC_DB_PASSWORD:?} \
-    --admin-username=${MAUTIC_ADMIN_USERNAME:?} \
-    --admin-email=${MAUTIC_ADMIN_EMAIL:?} \
-    --admin-password=${MAUTIC_ADMIN_PASSWORD:?} \
     --no-interaction \
     --force \
     ${SITE_URL:-"$@"}
